33b0f8144baaa6d03f415cea4a8491bd76472688
wonbinjeong <jeongwonbin0503@gmail.com>
2022-03-07 13:47:37 -0700

    feat: socket setup on server

--- code/server/src/api.js
+++ code/server/src/api.js
@@ -1,11 +1,36 @@
 import express from 'express';
 import cors from 'cors';
+import http from 'http';
+import SocketIO from 'socket.io';
 
-const api = express();
+const app = express();
 
-api.use(cors());
-api.use(express.json())
+app.use(cors());
+app.use(express.json())
 
-api.get('/', (_, res) => {
-  res.send('');
+app.get('/', (_, res) => {
+  res.status(200).send('');
 });
+
+app.get('/version', (_, res) => {
+  res.status(200).send("0.1.0");
+});
+
+const server = http.createServer(app);
+const wsServer = SocketIO(server);
+
+wsServer.on("connection", (socket) => {
+  socket.on("generate_room", (done) => {
+    // TODO: generate room id, playerid, etc.
+  })
+
+  socket.on("enter_room", (roomName, done) => {
+    socket.join(roomName);
+    done("Joined room");
+    socket.to(roomName).emit("welcome");
+  });
+
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach(room => socket.to(roon).emit("end_session"));
+  });
+})
