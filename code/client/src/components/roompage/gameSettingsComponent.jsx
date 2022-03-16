0b28d43ac97911b7c9a6c2d3c0e6e8b6e44a1e45
ghassanzadeh <golnoush.hz@gmail.com>
2022-03-16 12:29:51 -0600

    created UI for host and player settings v1

--- code/client/src/components/roompage/gameSettingsComponent.jsx
+++ code/client/src/components/roompage/gameSettingsComponent.jsx
@@ -2,10 +2,53 @@ import './gameSettingsComponent.css';
 
 const GameSettingsComponent = (props) => {
     return (
-        <>
-            <span class="game-title font-italic text-center">Host Settings</span>
-            <span class="game-title font-italic text-center">Player Settings</span>
-        </>
+        <div class="game-sections">
+            <div class="settings-vertical">
+                <div class="settings-content">
+                    <span class="game-title font-italic text-center">Host Settings</span>
+                        <div className='row'>
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                <span class="input-group-text" id="inputGroup-sizing-default">Set Speed:</span>
+                                </div>
+                                <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"></input>
+                            </div>
+                            <button class="button-71" role="button">Start Game</button>
+                        </div>
+                </div>
+                
+            </div>
+            <div class="settings-vertical">
+                <div class="settings-content">
+                    <span class="game-title font-italic text-center">Player Settings</span>
+                    <div className='row'>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="inputGroup-sizing-default">Nickname:</span>
+                            </div>
+                            <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"></input>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="inputGroup-sizing-default">Piece Color:</span>
+                            </div>
+                            <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"></input>
+                        </div>
+                    </div>
+                    {/* <div class="dropdown">
+                            <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-expanded="false">Piece Color
+                            </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                            <button class="dropdown-item" type="button">Action</button>
+                            <button class="dropdown-item" type="button">Another action</button>
+                            <button class="dropdown-item" type="button">Something else here</button>
+                        </div>
+                        </div> */}
+                </div>
+            </div>
+        </div>
     );
 }
 
