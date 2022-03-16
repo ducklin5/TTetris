6a639a9ea663ee128c2f61955cdd1ce5c843eff7
ghassanzadeh <golnoush.hz@gmail.com>
2022-03-16 12:49:04 -0600

    added a dummy chat box

--- code/client/src/components/roompage/chatboxComponent.jsx
+++ code/client/src/components/roompage/chatboxComponent.jsx
@@ -2,9 +2,34 @@ import './chatboxComponent.css';
 
 const ChatboxComponent = (props) => {
     return (
-        <>
-            <span class="chatbox-title font-italic text-center">Chat Box</span>
-        </>
+        <div class="settings-content">
+            <span class="chat-title font-italic text-center">Chat Box</span>
+            <div class="settings-vertical">
+                <div class="container">
+                    <p class="container-name">Player 1</p>
+                    <p>Hello. How are you ?</p>
+                    <span class="time-right">11:00</span>
+                </div>
+
+                <div class="container darker">
+                    <p class="container-name">Player 2</p>
+                    <p>Hey! I'm fine.</p>
+                    <span class="time-left">11:01</span>
+                </div>
+
+                <div class="container">
+                    <p class="container-name">Player 1</p>
+                    <p>what do you wanna do today?</p>
+                    <span class="time-right">11:02</span>
+                </div>
+                <div class="container darker">
+                    <p class="container-name">Player 2</p>
+                    <p>Play soccer</p>
+                    <span class="time-left">11:05</span>
+                </div>
+            </div>
+        </div>
+
     );
 }
 
