5591f60ac69dcb065da77381a0a98cbf30ebc93c
ghassanzadeh <golnoush.hz@gmail.com>
2022-03-18 15:28:33 -0600

    fix for chat box and player info scroll

--- code/client/src/components/roompage/playerInfoComponent.jsx
+++ code/client/src/components/roompage/playerInfoComponent.jsx
@@ -7,7 +7,7 @@ const PlayerInfoComponent = (props) => {
         <div className="players-content">
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"></link>
             <span className="player-title font-italic text-center">Players Info</span>
-            <div className="player-list">
+            <div className='player-list'>
                 <div className='player-info'>
                     <div className='player-name-button'>                  
                         <span className=" text-left font-weight-bold">Player-1</span>
@@ -18,20 +18,6 @@ const PlayerInfoComponent = (props) => {
                         <div className="player-piece"></div>
                     </div>
                 </div>
-
-
-
-                                <div className='player-info'>
-                    <div className='player-name-button'>                  
-                        <span className=" text-left font-weight-bold">Player-1</span>
-                        <button type="button" className="btn btn-link"><span className="bi bi-wifi"></span></button>
-                        <button type="button" className="btn btn-link"><span className="bi bi-mic"></span></button>
-                    </div>
-                    <div className="piece-container">
-                        <div className="player-piece"></div>
-                    </div>
-                </div>
-
             </div>
         </div>
     );
