e0468fbc833daece2b970fb87344f319069155f4
ghassanzadeh <golnoush.hz@gmail.com>
2022-03-21 16:15:36 -0600

    added sabotage button

--- code/client/src/pages/roompage/components/gameView/gameButtonsComponent.jsx
+++ code/client/src/pages/roompage/components/gameView/gameButtonsComponent.jsx
@@ -2,13 +2,20 @@ import './gameButtonsComponent.css';
 
 const GameButtonsComponent = (props) => {
     return (
-        <div className='emergency-timer'>
-            <button className='emergency-button'><i class="fa fa-exclamation-circle fa-lg"></i></button>
-            <div className='timer-frame'>
-                <div className='time-countdown'>1:15</div>
+        <div>
+            <div className='emergency-timer'>
+                <button className='emergency-button'><i class="fa fa-exclamation-circle fa-lg"></i></button>
+                <div className='timer-frame'>
+                    <div className='time-countdown'>1:15</div>
+                </div>
             </div>
+            <link href="https://emoji-css.afeld.me/emoji.css" rel="stylesheet"></link>
+            <button className='sabotage-button '><i class="em em-smiling_imp" aria-role="presentation" aria-label="SMILING FACE WITH HORNS"></i></button>
         </div>
+
     );
 }
 
 export default GameButtonsComponent;
+
+<i class="em em-smiling_imp" aria-role="presentation" aria-label="SMILING FACE WITH HORNS"></i>
