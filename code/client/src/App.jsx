19234652d03af6ee67dcf68ddef84df902c43852
Azeez Abass <abassazeez6@gmail.com>
2022-03-21 20:46:02 -0600

    fix(client): connect to websocket server using window.location

--- code/client/src/App.jsx
+++ code/client/src/App.jsx
@@ -6,7 +6,8 @@ import HelpPage from './pages/helppage/helppage';
 import { io } from "socket.io-client";
 
 const port = process.env.PORT || 8080;
-const socket = io.connect('http://127.0.0.1:'+port);
+const wsAddress = window.location.protocol + '//' + window.location.hostname + ':' +port
+const socket = io.connect(wsAddress);
 
 function App() {
   return (
