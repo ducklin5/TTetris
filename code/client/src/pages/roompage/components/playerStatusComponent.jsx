a90fb6e0b9b1b3c572ce4eae97c220e56ba41509
ghassanzadeh <golnoush.hz@gmail.com>
2022-03-23 15:59:09 -0600

    fixed some ui issues on room page

--- code/client/src/pages/roompage/components/playerStatusComponent.jsx
+++ code/client/src/pages/roompage/components/playerStatusComponent.jsx
@@ -8,7 +8,9 @@ const PlayerStatusComponent = (props) => {
             <div className='player-name-button'>                  
                 <span className=" text-left font-weight-bold">Player-1</span>
                 <button type="button" className="btn btn-link"><span className="bi bi-wifi fa-lg"></span></button>
+                {/* <button type="button" className="btn btn-link"><span className="bi bi-wifi-off fa-lg"></span></button> */}
                 <button type="button" className="btn btn-link"><span className="bi bi-mic fa-lg"></span></button>
+                {/* <button type="button" className="btn btn-link"><span className="bi bi-mic-mute fa-lg"></span></button> */}
             </div>
             <div className="piece-container">
                 <div className="player-piece"></div>
