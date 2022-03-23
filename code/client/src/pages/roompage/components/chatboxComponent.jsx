5bb7f4b405b5272449f2ef42025ed02b6865a89b
Azeez Abass <abassazeez6@gmail.com>
2022-03-23 17:27:19 -0600

    fix: make game input and message input coexist

--- code/client/src/pages/roompage/components/chatboxComponent.jsx
+++ code/client/src/pages/roompage/components/chatboxComponent.jsx
@@ -1,54 +1,86 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
-import './chatboxComponent.css';
-import MessageBoxComponent from './messageBoxComponent';
+import "./chatboxComponent.css";
+import MessageBoxComponent from "./messageBoxComponent";
 
 const ChatboxComponent = (props) => {
-    const {socket} = props;
+    const { socket } = props;
     const [message, setMessage] = useState("");
     const [chatHistory, setChatHistory] = useState([]);
     const roomID = useParams().roomID;
+    const sendButtonRef = useRef(null);
 
     useEffect(() => {
         socket.emit("getMessage", roomID, (chatHistory) => {
             setChatHistory(chatHistory);
-        })
-    },[])
+        });
+    }, []);
 
     socket.on("sendMessageAll", (chatHistory) => {
         setChatHistory(chatHistory);
-    })
+    });
 
     const onMessageChanged = (event) => {
         setMessage(event.target.value);
-    }
-    
+    };
+
     const onMessageSendClicked = () => {
-        socket.emit("sendMessage", roomID, message, window.clientID, () => {
-            setMessage("");
-        })
+        if (message.length) {
+            socket.emit("sendMessage", roomID, message, window.clientID, () => {
+                setMessage("");
+            });
+        }
+        sendButtonRef.current.blur();
+    };
+
+    const onInputKeyDown = (e) => {
+        if (["ArrowDown", "ArrowRight", "ArrowLeft", "ArrowUp"].includes(e.key)) {
+            return;
+        }
+        if(e.key == "Enter") {
+            onMessageSendClicked();
+        }
+        e.stopPropagation();
     }
 
     return (
         <div className="chatbox-content">
             <span className="chat-title font-italic text-center">Chat Box</span>
-            <div className='messages-container'>
-                {
-                    chatHistory.map(chat => 
-                        <MessageBoxComponent message={chat.message} nickname={chat.nickname} time={chat.time}/>
-                    )
-                }
+            <div className="messages-container">
+                {chatHistory.map((chat) => (
+                    <MessageBoxComponent
+                        message={chat.message}
+                        nickname={chat.nickname}
+                        time={chat.time}
+                    />
+                ))}
             </div>
-            <div className='send-box'>
+            <div className="send-box">
                 <div className="input-group mb-3">
-                    <input type="text" value={message} onChange={onMessageChanged} className="form-control" placeholder="Type..." aria-label="Type" aria-describedby="basic-addon2"></input>
+                    <input
+                        type="text"
+                        value={message}
+                        onKeyDownCapture={onInputKeyDown}
+                        onChange={onMessageChanged}
+                        className="form-control"
+                        placeholder="Type..."
+                        aria-label="Type"
+                        aria-describedby="basic-addon2"
+                    ></input>
                     <div className="input-group-append">
-                        <button onClick={onMessageSendClicked} className="btn send-button" type="button">Send</button>
+                        <button
+                            onClick={onMessageSendClicked}
+                            className="btn send-button"
+                            type="button"
+                            ref={sendButtonRef}
+                        >
+                            Send
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default ChatboxComponent;
