b7471f56d51510c1d3f5131a4eda68496da2a4ac
ghassanzadeh <golnoush.hz@gmail.com>
2022-03-25 14:57:45 -0600

    player's nickname is updated on player info

--- code/server/src/room/room_session.js
+++ code/server/src/room/room_session.js
@@ -48,6 +48,14 @@ class RoomSession {
         }
     }
 
+    changeClientName(clientID, newNickName) {
+        this.clients.forEach(client => {
+            if (client.id == clientID) {
+                client.nickname = newNickName;
+            }
+        })
+    }
+
     changeClientColor(clientID, newColor) {
         this.clients.forEach(client => {
             if (client.id == clientID) {
