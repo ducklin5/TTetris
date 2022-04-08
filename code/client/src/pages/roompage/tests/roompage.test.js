c3dc0dbe2897f3c54500106220e1d2528fd8c939
wonbinjeong <jeongwonbin0503@gmail.com>
2022-04-07 23:00:45 -0600

    test: added roompage tests

---
+++ code/client/src/pages/roompage/tests/roompage.test.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import MockedSocket from 'socket.io-mock';
+import RoomPage from 'pages/roompage/roompage';
+import { Routes, Route, MemoryRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import GameViewComponent from '../components/gameView/gameViewComponent';
+import GameSettingsComponent from '../components/gameSettingsComponent';
+
+describe("Testing Room Page", () => {
+    let socket;
+
+    beforeEach(() => {
+        socket = new MockedSocket();
+    })
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    })
+
+    test("Should show up", () => {
+        const tree = renderer
+            .create(<MemoryRouter>
+                <Routes>
+                    <Route path="/" element={<RoomPage socket={socket.socketClient}/>} />
+                </Routes>
+            </MemoryRouter>)
+            .toJSON();
+
+        expect(tree).toMatchSnapshot();
+    })
+
+    test("Should show settings component", async () => {
+        const component = renderer
+            .create(<MemoryRouter>
+                <Routes>
+                    <Route path="/" element={<RoomPage socket={socket.socketClient}/>} />
+                </Routes>
+            </MemoryRouter>)
+
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+
+        let a = await component.root.findAllByProps({socket: socket.socketClient});
+        expect(a[4]._fiber.elementType).toEqual(GameSettingsComponent);
+    })
+
+    test("Should show game view component", async () => {
+        window.clientID = "abcd";
+        window.gameData = {players: {"abcd":{
+            nickname: "abc",
+            color: "#FFF",
+            init_ofx: 0,
+            currentPiece: null,
+            isImposter: false,
+            hasEmergency: true,
+            isExiled: false,
+        }}};
+        
+        const component = renderer
+            .create(<MemoryRouter>
+                <Routes>
+                    <Route path="/" element={<RoomPage socket={socket.socketClient}/>} />
+                </Routes>
+            </MemoryRouter>)
+
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+
+        let a = await component.root.findAllByProps({socket: socket.socketClient});
+        expect(a[4]._fiber.elementType).toEqual(GameViewComponent);
+    })
+})
